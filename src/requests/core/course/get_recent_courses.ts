5eb393735e0245a20b6e23622691de06e840b0ef
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-19 20:01:40 +0200

    chore(requests): add comments for `core_course_get_recent_courses`

--- src/requests/core/course/get_recent_courses.ts
+++ src/requests/core/course/get_recent_courses.ts
@@ -1,21 +1,43 @@
 import { type RecentCourseFromAPI, RecentCourse } from "~/models/RecentCourse";
 
 interface Parameters {
-  userid: number
-  limit: number
+  /**
+   * User ID from which the courses will be obtained.
+   * @default 0 // defaults to current user.
+   */
+  userid?: number
+
+  /**
+   * Restrict result set to this amount.
+   * @default 0
+   */
+  limit?: number
+
+  /**
+   * Skip this number of records from the start of the result set.
+   * @default 0
+   */
+  offset?: number
+
+  /**
+   * SQL string for sorting.
+   * @default null
+   */
+  sort?: string | null
 }
 
 type Response = Array<RecentCourseFromAPI>;
 
 /**
- * TODO: description
+ * Get last accessed courses adding additional course information like images.
+ *
+ * @location `/course/externallib.php`
+ * @method `get_recent_courses`
  */
-const core_course_get_recent_courses = {
+export default {
   schema: {} as Parameters,
   name: "core_course_get_recent_courses",
   handle (response: Response): Array<RecentCourse> {
     return response.map(RecentCourse.fromAPI);
   }
 };
-
-export default core_course_get_recent_courses;
