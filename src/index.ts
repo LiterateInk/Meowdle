e00521c23de14c6452367ba13f37548759d02e2d
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-19 18:53:46 +0200

    chore: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-export { Session } from "~/models/Session";
 export { Token } from "~/models/Token";
-export { WebServiceToken } from "~/models/WebServiceToken";
 export { RSSToken } from "~/models/RSSToken";
+export { WebServiceToken } from "~/models/WebServiceToken";
+export { Session } from "~/models/Session";
+export { UserGroup } from "~/models/UserGroup";
+export { EnrolledUser } from "~/models/EnrolledUser";
+export { RecentCourse } from "~/models/RecentCourse";
 
+// Clients that will be mostly used
+export { AJAX } from "~/services/AJAX";
 export { Client } from "~/services/Client";
+export { WebService } from "~/services/WebService";
+
+// Sub-services that can be used as type, probably.
 export { Authenticator } from "~/services/Authenticator";
 export { Preferences } from "~/services/Preferences";
 export { TokenManager } from "~/services/TokenManager";
 
+// Builder functions for AJAX or WebService.
+export * as requests from "~/requests";
