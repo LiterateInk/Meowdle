75c3a27a477637ade96a185393d0fdffdcaa7189
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-19 18:50:56 +0200

    feat(services): rewrite AJAX client for new requests structure

--- src/services/AJAX.ts
+++ src/services/AJAX.ts
@@ -1,9 +1,47 @@
-import { Client } from "~/services/Client";
+import type { MethodMap, Methods } from "~/requests";
+import type { Client } from "~/services/Client";
 
 export class AJAX {
   readonly #client: Client;
 
   public constructor (client: Client) {
     this.#client = client;
+  }
+
+  public from<N extends Methods["name"]>(method: Methods & { name: N }) {
+    return {
+      request: async (params: MethodMap[N]["schema"]): Promise<ReturnType<MethodMap[N]["handle"]>> => {
+        let url = `${this.#client.session.root}/lib/ajax/service.php`;
+        url += `?sesskey=${this.#client.session.sessionKey}`;
+        url += `&info=${method.name}`;
+
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Cookie": this.#client.cookies.join("; "),
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify([{
+            index: 0,
+            methodname: method.name,
+            args: params
+          }])
+        });
+
+        const json = await response.json() as [{
+          error: false,
+          data: any // this is what we want
+        }] | {
+          error: string
+          errorcode: string
+        };
+
+        if ("error" in json && "errorcode" in json) {
+          throw new Error(`(${json.errorcode}): ${json.error}`);
+        }
+
+        return method.handle(json[0].data) as ReturnType<MethodMap[N]["handle"]>;
+      }
+    };
   }
 }
