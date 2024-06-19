36e2e03f2bd6d2482d06ca1c8675d95190aa9811
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-19 21:36:37 +0200

    docs(examples): add custom request

---
+++ examples/custom_request.ts
@@ -0,0 +1,48 @@
+import { credentials } from "./_credentials";
+import { WebService, type RequestHandler } from "../src";
+
+interface MyRequestParameters {
+  user_id: number
+  something_random: string
+}
+
+interface MyRequestResponseFromAPI {
+  hello: string
+  world: number
+}
+
+type MyRequestReturn = string;
+
+const my_request = {
+  // Is supported by AJAX endpoint ?
+  ajax: false,
+  // Should be authenticated ?
+  authenticated: true,
+
+  // An handler that will be called when the request is done.
+  // Used to transform the response from the API to the return value.
+  handle(response) {
+    return response.hello + " " + response.world;
+  },
+
+  name: "my_request",
+  // Yes, pass an empty object, it's just for typings.
+  // (if anyone knows how to make it cleaner, please open an issue or PR !)
+  schema: <MyRequestParameters>{}
+} satisfies RequestHandler<MyRequestParameters, MyRequestResponseFromAPI, MyRequestReturn>;
+
+void async function main () {
+  if (!credentials.ws_token)
+    throw new Error("Missing the WS_TOKEN in .env file.");
+
+  const ws = new WebService(credentials.ws_token, credentials.root);
+
+  // Use your custom `my_request` here.
+  const returnValue = await ws.from(my_request).request({
+    // You get intellisense here, properties are from `MyRequestParameters`.
+    something_random: "random",
+    user_id: 69420
+  });
+
+  console.log(returnValue); // Return type corresponds to `MyRequestReturn`.
+}();
