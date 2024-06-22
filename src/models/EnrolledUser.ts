40135e8ba1f10ea29549324f8c853f3b086439b2
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-23 00:57:12 +0200

    chore(models): wip for EnrolledUser

---
+++ src/models/EnrolledUser.ts
@@ -0,0 +1,14 @@
+export interface EnrolledUserFromAPI {
+  // TODO
+}
+
+export class EnrolledUser {
+  public constructor (
+  ) {}
+
+  public static fromAPI (data: EnrolledUserFromAPI): EnrolledUser {
+    // TODO
+    return new EnrolledUser(
+    );
+  }
+}
